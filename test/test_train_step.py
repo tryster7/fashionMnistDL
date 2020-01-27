f637d4e6956f83e7429c34dd3ea8467323468b16
tryster7 <59949804+tryster7@users.noreply.github.com>
2020-01-27 17:15:01 +0530

    [skip ci]

--- test/test_train_step.py
+++ test/test_train_step.py
@@ -8,18 +8,17 @@ from train import train
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self) -> None:
-        (trainX, trainy), (testX, testy) = fashion_mnist.load_data()
-        self.testy = testy
-        self.trainX = trainX / 255
-        self.testX = testX / 255
-        self.trainX = self.trainX.reshape(trainX.shape[0], 28, 28, 1)
-        self.testX = self.testX.reshape(testX.shape[0], 28, 28, 1)
-        self.trainY = trainy
+    #def setUp(self) -> None:
+    #    (trainX, trainy), (testX, testy) = fashion_mnist.load_data()
+    #    self.testy = testy
+    #    self.trainX = trainX / 255
+    #    self.testX = testX / 255
+    #    self.trainX = self.trainX.reshape(trainX.shape[0], 28, 28, 1)
+    #    self.testX = self.testX.reshape(testX.shape[0], 28, 28, 1)
+    #    self.trainY = trainy
 
     def test_loadmodel_and_predict(self):
-        model = tf.saved_model.load('gs://a-kb-poc-262417/mnist/export/model/1')
-       
+        model = tf.saved_model.load('gs://a-kb-poc-262417/mnist/export/model/1')     
         #predictions = model.predict(self.trainX[0])
         #label = np.argmax(predictions, axis=1)
         #print(label)
